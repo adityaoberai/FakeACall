30e24f5f081f77b249bfe6830bbbf40b516c1a8c
Aditya Oberai <adityaoberai1@gmail.com>
2021-01-13 21:48:30 +0530

    Created initial app

---
+++ FakeACall/FakeACall/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Wearable.Views;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using Android.Support.Wearable.Activity;
+using Java.Interop;
+using Android.Views.Animations;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using Twilio.TwiML;
+
+namespace FakeACall
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : WearableActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+
+            button.Click += delegate {
+                const string accountSid = "ACe29c144db149972dbf5427bbdd0c16dd";
+                const string authToken = "661848e78edfd1af8e5f98a0ab94bea6";
+                TwilioClient.Init(accountSid, authToken);
+
+                var to = new PhoneNumber("+918439056262");
+                var from = new PhoneNumber("+12184232045");
+                var call = CallResource.Create(to, from,
+                    twiml: new Twiml("<Response><Say>Hey there. Hope you're doing well. Well done getting this call running. Use this opportunity to get away from here.</Say></Response>"));
+
+                //Console.WriteLine(call.Sid);
+            };
+
+            SetAmbientEnabled();
+        }
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+## FakeACall
+
+FakeACall is a Android Wear application that allows the smartwatch owner to fake a phone call in order to get out of situations they may want to. It has been built using Xamarin.Android and uses Twilio Programmable Voice to fake a phone call.
