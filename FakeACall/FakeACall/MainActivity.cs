cc70478a225054c413f3e7fd1e085e6866512d1e
Aditya Oberai <adityaoberai1@gmail.com>
2021-02-16 10:49:33 +0530

    Removed non functional Azure Function URL and added placeholder

--- FakeACall/FakeACall/MainActivity.cs
+++ FakeACall/FakeACall/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Net.Http;
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -10,49 +11,62 @@ using Android.Support.Wearable.Views;
 using Android.Support.V4.App;
 using Android.Support.V4.View;
 using Android.Support.Wearable.Activity;
-using Java.Interop;
 using Android.Views.Animations;
-using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Threading;
+using Java.Interop;
 
 namespace FakeACall
 {
     [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : WearableActivity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected override async void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.activity_main);
 
             Button button = FindViewById<Button>(Resource.Id.button1);
 
-            button.Click += async delegate
+            button.Text = "Attempting Call";
+            if(await Call()==true)
+            { 
+                button.Text = "Call Successful";
+            }
+            
+            else
             {
-                button.Text = "Attempting Call";
-                await Call();
-            };
-            button.Text = "Call Now";
+                button.Text = "Call Failed";
+            }
+
+            CloseApp();
 
             SetAmbientEnabled();
         }
 
-        private async Task Call()
+        private async Task<bool> Call()
         {
             try
             {
                 HttpClient client = new HttpClient();
-                HttpResponseMessage response = await client.PostAsync("https://fakeacall.azurewebsites.net/api/CallNumber", null);
+                HttpResponseMessage response = await client.PostAsync("<ADD AZURE FUNCTION URL HERE>", null);
+                return true;
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error Occured\n\n" + ex.Message);
-            }
-            
+                return false;
+            }   
         }
 
+        private void CloseApp()
+        {
+            Handler h = new Handler();
+            Action close = () =>
+            {
+                Process.KillProcess(Process.MyPid());
+            };
+
+            h.PostDelayed(close, 3000);
+        }
     }
 }
 
